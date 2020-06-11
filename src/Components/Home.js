a7ff033cff464bf0582b2040a22436b590160b2e
James Riches <jameseldonriches@gmail.com>
2020-06-11 14:30:51 -0800

    minor edits

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,9 +12,8 @@ class Home extends React.Component {
         ],
       };
       addPost = (postData) => {
-        const post = { id: `Math.random()`, ...postData };
-        // const post =
-        // {id: `Math.random()`, title: postData.title, body:postData.body }
+        // const post = { id: `Math.random()`, ...postData };
+        const post = {id: `Math.random()`, title: postData.title, body:postData.body }
         const newPosts = [post, ...this.state.posts];
         this.setState({
           posts: newPosts,
@@ -22,7 +21,7 @@ class Home extends React.Component {
       };
       renderPosts() {
         return this.state.posts.map((post) => {
-          // return <Post key={post.id} title={post.title} body={post.body} id={post.id} />;
+        //   return <Post key={post.id} title={post.title} body={post.body} id={post.id} />;
           return <Posts key={post.id} {...post} />;
         });
       }
